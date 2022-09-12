d81350788937cb76eefa52acd9defcb6b8dc783d
merey <mereika1712@gmail.com>
2022-09-12 16:18:17 +0600

    change

--- src/main/java/com/example/javafxlesson1/HelloApplication.java
+++ src/main/java/com/example/javafxlesson1/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello! 3E");
+        stage.setTitle("Hello! 3I");
         stage.setScene(scene);
         stage.show();
     }
